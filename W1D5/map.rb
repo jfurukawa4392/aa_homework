c49dcf1e1a37e9d9e8fba2e69f45107b9746c163
Jesse Furukawa <tuc64377@temple.edu>
2017-01-26 22:26:55 -0800

    add stack, queue and map ADTs for W1D5 hw

---
+++ W1D5/map.rb
@@ -0,0 +1,57 @@
+class Map
+
+  def initialize
+    @ivar = []
+  end
+
+  def assign(key, value)
+    if self.lookup(key)
+      pair.pop
+      pair.push(value)
+    else
+      @ivar.push([key, value])
+    end
+  end
+
+  def lookup(key)
+    @ivar.each do |pair|
+      if pair.first == key
+        return pair.dup
+      end
+    end
+
+    nil
+  end
+
+  def remove(key)
+    @ivar.delete_if { |pair| pair.first == key }
+  end
+
+  def show
+    print "#{@ivar} \n"
+  end
+end
+
+if __FILE__ == $PROGRAM_NAME
+  map = Map.new
+  map.assign(2,1)
+  map.show
+  sleep(1)
+  map.assign(1,2)
+  map.show
+  sleep(1)
+  map.assign(3,3)
+  map.show
+  sleep(1)
+  map.assign(5,1)
+  map.show
+  sleep(1)
+  map.remove(5)
+  map.show
+  sleep(1)
+  map.remove(1)
+  map.show
+  sleep(1)
+  map.assign(1,10)
+  map.show
+end

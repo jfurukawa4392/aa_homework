7014feb3582f53b953974e2e8027597a8a10de9b
Jesse Furukawa <tuc64377@temple.edu>
2017-02-05 15:00:15 -0800

    add aggregate function sql exercises

--- W3D1/sql zoo/lib/05_aggregates.rb
+++ W3D1/sql zoo/lib/05_aggregates.rb
@@ -42,19 +42,31 @@ end
 def area_count
   # How many countries have an area of more than 1,000,000?
   execute(<<-SQL)
-  
+  select
+  count(name)
+  from countries
+  where area > 1000000
   SQL
 end
 
 def group_population
   # What is the total population of ('France','Germany','Spain')?
   execute(<<-SQL)
+  select
+  sum(population)
+  from countries
+  where name in ('France','Germany','Spain')
   SQL
 end
 
 def country_counts
   # For each continent show the continent and number of countries.
   execute(<<-SQL)
+  select
+  continent,
+  count(name)
+  from countries
+  group by continent
   SQL
 end
 
@@ -62,11 +74,27 @@ def populous_country_counts
   # For each continent show the continent and number of countries with
   # populations of at least 10 million.
   execute(<<-SQL)
+  select
+  continent,
+  count(name)
+  from countries
+  where population >= 10000000
+  group by continent
   SQL
 end
 
 def populous_continents
   # List the continents that have a total population of at least 100 million.
   execute(<<-SQL)
+  select
+  a.continent
+  from (
+    select
+    continent,
+    sum(population) as population
+    from countries
+    group by continent
+  ) a
+  where a.population >= 100000000
   SQL
 end

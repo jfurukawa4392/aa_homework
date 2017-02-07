79f4dedc19c29133e9b09596ab20181fc6305c28
Jesse Furukawa <tuc64377@temple.edu>
2017-02-06 20:52:22 -0800

    add playwright CRU functionality with class and instance SQL calls

--- W3D2/plays_demo/plays.rb
+++ W3D2/plays_demo/plays.rb
@@ -77,20 +77,60 @@ class Playwright
   attr_accessor :name, :birth_year
 
   def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Playwright.new(datum) }
   end
 
   def self.find_all_by_name(name)
+    data = PlayDBConnection.instance.execute(<<-SQL, name)
+    SELECT
+    *
+    FROM playwrights
+    WHERE name = ?
+    SQL
+    data.map { |playwright| Playwright.new(playwright) }
   end
 
-  def initialize
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
   end
 
   def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.execute(<<-SQL, @name, @birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
   end
 
   def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
   end
 
   def get_plays #get_plays (returns all plays written by playwright)
+    raise "#{self} not in database" unless @id
+    plays = PlayDBConnection.instance.execute(<<-SQL, @id)
+      SELECT DISTINCT
+      plays.*
+      FROM plays
+      JOIN playwrights
+      ON plays.playwright_id = playwrights.id
+      WHERE plays.playwright_id = @id
+    SQL
+
+    plays.map! { |play| Play.new(play) }
   end
 end

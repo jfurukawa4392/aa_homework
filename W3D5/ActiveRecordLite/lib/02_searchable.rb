38b2d1f1a68b2e1fc32d389e18c39b3e25cf61b5
Jesse Furukawa <tuc64377@temple.edu>
2017-02-10 15:11:12 -0800

    add Searchable module to base ORM object for hash-accepting where clause

--- W3D5/ActiveRecordLite/lib/02_searchable.rb
+++ W3D5/ActiveRecordLite/lib/02_searchable.rb
@@ -3,10 +3,24 @@ require_relative '01_sql_object'
 
 module Searchable
   def where(params)
-    # ...
+    where_line = params.map do |k, v|
+      "#{k} = ?"
+    end.join(' AND ')
+
+    results = DBConnection.execute(<<-SQL, params.values)
+    SELECT
+      *
+    FROM
+      #{self.table_name}
+    WHERE
+      #{where_line}
+    SQL
+
+    results.map { |el| self.new(el) }
   end
 end
 
 class SQLObject
   # Mixin Searchable here...
+  extend Searchable
 end

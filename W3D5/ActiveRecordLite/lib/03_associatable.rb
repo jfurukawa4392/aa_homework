a2f3186a5e09315568d3edd223f865d8a9879f4c
Jesse Furukawa <tuc64377@temple.edu>
2017-02-10 16:42:31 -0800

    add default association class for Associatable module

--- W3D5/ActiveRecordLite/lib/03_associatable.rb
+++ W3D5/ActiveRecordLite/lib/03_associatable.rb
@@ -14,13 +14,21 @@ class AssocOptions
   end
 
   def table_name
-    # ...
+    @table_name = table_name
   end
 end
 
 class BelongsToOptions < AssocOptions
   def initialize(name, options = {})
-    # ...
+    defaults = {class_name: name.camelcase, primary_key: :id,
+                foreign_key: "#{name}_id".to_sym}
+
+    overridden = defaults.merge(options)
+
+    @class_name = overridden[:class_name]
+    @primary_key = overridden[:primary_key]
+    @foreign_key = overridden[:foreign_key]
+    @table_name = self.class.table_name
   end
 end
 
@@ -33,7 +41,11 @@ end
 module Associatable
   # Phase IIIb
   def belongs_to(name, options = {})
-    # ...
+    opts = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      fk = opts.send(:foreign_key)
+    end
   end
 
   def has_many(name, options = {})

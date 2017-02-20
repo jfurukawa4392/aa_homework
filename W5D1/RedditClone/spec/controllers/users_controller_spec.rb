fdad0a00091320edc6443f24c1c6dbaef1d144c2
Jesse Furukawa <tuc64377@temple.edu>
2017-02-20 01:16:35 -0800

    add controller testing to users controller

--- W5D1/RedditClone/spec/controllers/users_controller_spec.rb
+++ W5D1/RedditClone/spec/controllers/users_controller_spec.rb
@@ -1,24 +1,32 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe UsersController, :type => :controller do
+
+  describe "GET #new" do
+    it "renders the new template" do
+      get :new, {}
+      expect(response).to render_template("new")
+    end
+  end
+
   describe "POST #create" do
     context "with invalid params" do
       it "validates the presence of the user's username and password" do
-        post :create, user: {name: "jack_bruce", password: ""}
+        post :create, { user: {username: '', password: ''} }
         expect(response).to render_template("new")
         expect(flash[:errors]).to be_present
       end
 
       it "validates that the password is at least 6 characters long" do
-        post :create, user: {name: "jack_bruce", password: "short"}
+        post :create, { user: {username: 'jesse', password: 'hello'} }
         expect(response).to render_template("new")
         expect(flash[:errors]).to be_present
       end
     end
 
     context "with valid params" do
-      it "redirects user to subs index on success" do
-        post :create, user: {name: "jack_bruce", password: "password"}
+      it "redirects user to links index on success" do
+        post :create, { user: {username: 'jesse', password: 'password'} }
         expect(response).to redirect_to(subs_url)
       end
     end

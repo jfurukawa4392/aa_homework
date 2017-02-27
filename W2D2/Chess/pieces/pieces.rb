a8856ade7100d513c730e4efce9f877e4af918b2
Jesse Furukawa <tuc64377@temple.edu>
2017-02-26 19:29:28 -0800

    remove old folders from this branch and update css assets

--- W2D2/Chess/pieces/pieces.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'null_piece'
-require_relative 'bishop'
-require_relative 'rook'
-require_relative 'queen'
-require_relative 'king'
-require_relative 'knight'
-require_relative 'pawn'
--- W3D3/PanchoSan/db/migrate/20170208053048_create_houses.rb
+++
@@ -1,8 +0,0 @@
-class CreateHouses < ActiveRecord::Migration
-  def change
-    create_table :houses do |t|
-      t.string :address, null: false
-      t.timestamp
-    end
-  end
-end

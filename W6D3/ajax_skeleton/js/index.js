e103f6c832a38abde22df598977e337f9132bdec
Jesse Furukawa <tuc64377@temple.edu>
2017-02-28 17:06:50 -0800

    make ajax request for weather

--- W6D3/ajax_skeleton/js/index.js
+++ W6D3/ajax_skeleton/js/index.js
@@ -1,5 +1,19 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+// $.ajax({
+//   url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+//   type: "GET",
+//   success: function(res) {
+//     console.log(res['weather'][0]['description']);
+//   }
+// });
 
+$.get({
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success: function(res) {
+    console.log(res['weather'][0]['description']);
+  }
+});
 // Add another console log here, outside your AJAX request
+console.log("finished");

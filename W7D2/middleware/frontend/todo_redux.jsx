8334dbcac0e11669a96b8cd6bd2eb2d69ca60401
Jesse Furukawa <tuc64377@temple.edu>
2017-03-06 21:37:38 -0800

    initial commit for middleware todo list homework

---
+++ W7D2/middleware/frontend/todo_redux.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import configureStore from './store/store';
+
+import Root from './components/root';
+
+
+
+function applyMiddlewares(store, ...middleWares) {
+  let dispatch = store.dispatch;
+  middleWares.forEach((mW) => {
+    dispatch = mW(store)(dispatch);
+  });
+
+  return Object.assign({}, store, { dispatch });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const preloadedState = localStorage.state ?
+    JSON.parse(localStorage.state) : {};
+  let store = configureStore(preloadedState);
+
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
+});
